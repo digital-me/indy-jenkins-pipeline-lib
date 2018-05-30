e29e1cb8765ee20e079f9b4c7873c55b67cf4c18
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2018-05-30 20:12:13 +0200

    Optimize common and install scripts
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- resources/lazyDir/common.sh
+++ resources/lazyDir/common.sh
@@ -7,6 +7,19 @@ set -o pipefail
 : ${DIST:="${LAZY_LABEL}"}
 : ${WORKSPACE:="${PWD}"}
 
+# Set some dry-run options/arguments
+if [ -n "${DRYRUN}" -a "${DRYRUN}" != '0' -a "${DRYRUN}" != 'false' ]; then
+	DRY_CMD='echo '
+	DRY_ARG='-n'
+fi
+
+# Some general useful commands and variables
+GREP='/bin/grep'
+GZIP='/bin/gzip'
+: ${RSYNC_OPTIONS:="-hal --stats --exclude=\"lost+found\""}
+RSYNC="/usr/bin/rsync ${RSYNC_OPTIONS}"
+SUDO='/usr/bin/sudo'
+
 # Figure out the distro name if not supplied
 if [ -z "${DIST}" ]; then
 	if [ -x /usr/bin/lsb_release ]; then
@@ -21,30 +34,24 @@ fi
 # Give up if the distro is still unknown
 [ -n "${DIST}" -o "${DIST}" = '-' ] || { echo "Undefined distribution!"; exit 1; }
 
-# Set some dry-run options/arguments
-if [ -n "${DRYRUN}" -a "${DRYRUN}" != '0' -a "${DRYRUN}" != 'false' ]; then
-	DRY_CMD='echo '
-	DRY_ARG='-n'
-fi
-
+# Some distro specific command
 case "${DIST}" in
 	centos*)
-		SUDO='/usr/bin/sudo'
 		PYTHON='/bin/python3.5'
 		PYTHON_PREFIX="$(rpm -q --whatprovides ${PYTHON} --queryformat '%{name}' 2> /dev/null | cut -d'-' -f1 || echo 'python35u')"
 		PIP='/bin/pip3.5'
+		YUM="/usr/bin/yum"
 		PKG_EXT='rpm'
-		PKG_MNG="$SUDO /usr/bin/yum"
-		YUM="$PKG_MNG"
+		PKG_MNG="$SUDO $YUM"
 	;;
 	debian*|ubuntu*)
 		SUDO='/usr/bin/sudo'
 		PYTHON='/usr/bin/python3.5'
 		PYTHON_PREFIX="$(dpkg-query --search ${PYTHON} 2> /dev/null | cut -d'-' -f1 || echo '')"
 		PIP='/usr/bin/pip3'
+		APT="/usr/bin/apt"
 		PKG_EXT='deb'
-		PKG_MNG="$SUDO /usr/bin/apt-get"
-		APT="$PKG_MNG"
+		PKG_MNG="$SUDO $APT-get"
 	;;
 	*)
 		echo "Unknown distribution (= ${DIST})"
@@ -52,9 +59,6 @@ case "${DIST}" in
 	;;
 esac
 
-# Some general useful reusable commands and variables
+# Some extra reusable variables
 PYTHON_VER_STR="$(${PYTHON} --version 2> /dev/null || echo 'unknown')"
 PYTHON_VER="${PYTHON_VER_STR##* }"
-: ${RSYNC_OPTIONS:="-hal --stats --exclude=\"lost+found\""}
-RSYNC="/usr/bin/rsync ${RSYNC_OPTIONS}"
-GZIP='/bin/gzip'
--- resources/lazyDir/publish/install.sh
+++ resources/lazyDir/publish/install.sh
@@ -11,13 +11,13 @@ source "${SDIR}/common.sh" || source "${SDIR}/../common.sh"
 # Add a local repo and verify the installation with dependencies
 case "${DIST}" in
 	centos*)
-		$YUM -y clean expire-cache
-		$YUM-config-manager --add-repo "file://${DIST_DIR}/"
-		$YUM -y install ${1}
+		$SUDO $YUM -y clean expire-cache
+		$SUDO $YUM-config-manager --add-repo "file://${DIST_DIR}/"
+		$SUDO $YUM -y install ${1}
 	;;
 	debian*|ubuntu*)
-		$APT-get -y update
-		$APT-add-repository -y -u "file://${DIST_DIR}/"
-		$APT-get -y install ${1}
+		$SUDO $APT-get -y update
+		$SUDO $APT-add-repository -y -u "file://${DIST_DIR}/"
+		$SUDO $APT-get -y install ${1}
 	;;
 esac
