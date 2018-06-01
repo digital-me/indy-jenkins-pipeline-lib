dad2119981c0e66d23dd3ffa8b71367684f7009c
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2018-06-01 13:20:53 +0200

    Replace remote repo by local one rather than duplicate
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- resources/lazyDir/publish/install.sh
+++ resources/lazyDir/publish/install.sh
@@ -10,12 +10,13 @@ source "${SDIR}/common.sh" || source "${SDIR}/../common.sh"
 # Add a local repo and verify the installation with dependencies
 case "${DIST}" in
 	centos*)
-		$SUDO $YUM-config-manager --add-repo "file://${WORKSPACE}/${BUILD_DIR}/dists/${DIST}/${REPO_BRANCH}"
+		$SUDO $YUM-config-manager --save indy --setopt "indy.baseurl=file://${WORKSPACE}/${BUILD_DIR}/dists/${DIST}/${REPO_BRANCH}"
 		$SUDO $YUM -y clean expire-cache
 		$SUDO $YUM -y install ${1}
 	;;
 	debian*|ubuntu*)
-		$SUDO $APT-add-repository -y -u "deb file://${WORKSPACE}/${BUILD_DIR}/ ${DIST} ${REPO_BRANCH}"
+		$SUDO $SED -r -e "s;^deb\s+${REPO_BASEURL}.+\$;deb file://${WORKSPACE}/${BUILD_DIR}/ ${DIST} ${REPO_BRANCH};" \
+			-i /etc/apt/sources.list.d/indy.list
 		$SUDO $APT-get -y update
 		$SUDO $APT-get -y --allow-unauthenticated install ${1}
 	;;
